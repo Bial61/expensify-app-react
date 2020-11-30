44dbcc54492d5f4d85207a2c19d1dabbc1a2f2ad
Bial61 <freelancer6173@gmail.com>
2020-11-30 03:47:05 -0800

    =3rd commit prod

--- src/app.js
+++ src/app.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
 import AppRouter from './routers/AppRouter';
 import configStore from './redux/store/store'
-import {addExpense} from './redux/actions/expenses'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configStore () ;
-
+ 
 const jsx= (
     <Provider store={store}>
        <AppRouter/>
